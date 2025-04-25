9be16ddf22eff23c55c2a99fc2f6ee4d7c15fee4
Jeremy Taylor <jtaylor@jctaylor.org>
2025-04-25 13:44:05 -0500

    Initial Commit

---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Jeremy Taylor on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
